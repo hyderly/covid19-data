34ac6e05ea4dd3c16dacde21cbb07eca71ed5ae4
haider <haidertiger10@gmail.com>
2020-03-30 02:17:14 +0500

    Updates

---
+++ precache-manifest.02fc2177031afe692da219dc67b9e0be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b67f7b8e0f1e3a5eaee3287c677ef825",
+    "url": "/covid--data/index.html"
+  },
+  {
+    "revision": "096d7c59e339668e4f48",
+    "url": "/covid--data/static/css/main.08e1a1db.chunk.css"
+  },
+  {
+    "revision": "985978a8db945a51c594",
+    "url": "/covid--data/static/js/2.4627fd1e.chunk.js"
+  },
+  {
+    "revision": "096d7c59e339668e4f48",
+    "url": "/covid--data/static/js/main.37f54eda.chunk.js"
+  },
+  {
+    "revision": "b535d5d87b08f960741f",
+    "url": "/covid--data/static/js/runtime~main.a7e56ef2.js"
+  }
+]);
